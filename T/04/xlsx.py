315d6a751389478e7a66bc6231fae893ef39f272
kangdonguk <kadragon@sasa.hs.kr>
2019-06-08 11:00:13 +0900

    fix: xlsx.py fix

--- T/04/xlsx.py
+++ T/04/xlsx.py
@@ -7,23 +7,34 @@
 # xlsx 를 읽기 위한 코드
 import openpyxl
 
-
 # 엑셀 파일 열기
 wb = openpyxl.load_workbook('전국도서관표준데이터.xlsx')
-
-# sheet 의 이름 목록을 구함
-# gsn = wb.sheetnames
 
 # 활성 sheet 선택하기
 ws = wb.active
 
 i = 0
+lib_cnt = {}
 
 for row in ws.rows:
+    # 어떤 정보가 들어 있는지 확인하기 위한 코드
     if i == 0:
+        # print(', '.join(row))
         for k in row:
             print(k.value, end=',')
 
+        print()  # 줄바꿈 한번 하기
         i += 1
+        continue
 
-    print(row[1].value)
+    place = row[1].value
+
+    if place in lib_cnt:
+        lib_cnt[place] += 1
+    else:
+        lib_cnt[place] = 1
+
+for key in sorted( list( lib_cnt.keys() )):
+    print(key, lib_cnt[key])
+
+
